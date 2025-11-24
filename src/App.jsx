77f0cf940056fbc16303c9380435412566bca005
tejash098 <jaitej123@gmail.com>
2025-11-25 00:32:41 +0530

    adding react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ExpenseDashboard from './Components/ExpenseDashboard'
+function App() {
+
+  return (
+    <>
+    <ExpenseDashboard />
+    </>
+  )
+}
+
+export default App
